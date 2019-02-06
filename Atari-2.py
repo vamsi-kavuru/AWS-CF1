9a99fa2cd06233d0f2491af94b78a0c717fab267
Vamsi.Kavuru <vamsi.kavuru@gmail.com>
2019-02-05 19:31:23 -0500

    Added a python file which is for Atari game.

---
+++ Atari-2.py
@@ -0,0 +1,34 @@
+from __future__ import print_function
+print ("welcome to my atari adventure game! the directions to move in are defined as left, right, up, and down. Enjoy.")
+
+x = 6
+y = -2
+
+#move = str(input('Make your move! Type in either "Left", "Right", "Up", "Down" or type "Exit" to exit the game:')).lower()
+
+def game():
+    global x
+    global y
+    move = str(input('Make your move! Type in either "Left", "Right", "Up", "Down" or type "Exit" to exit the game:')).lower()
+    if move == ('left'):
+        x = x-2
+        print ('your x position is', x, '.' 'your y position is', y, '.')
+        game()
+    elif move == ('up'):      
+        y = y+2
+        print ('your x position is', x, '.' 'your y position is', y, '.')
+        game()
+    elif move == ('right'):
+        x = x+2
+        print ('your x position is', x, '.' 'your y position is', y, '.')
+        game()
+    elif move == ('down'):
+        y = y-2
+        print ('your x position is', x, '.' 'your y position is', y, '.')
+        game()
+    elif move == ('exit'):
+        exit()
+    else:
+        print ("Oops!!! You have entered a wrong input, please enter either up, down, left, right or exit only.")
+        game()
+game()
